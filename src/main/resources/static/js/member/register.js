9a9317e2b7ec7715ca8875240356f7510d9a7528
kjw1237 <kjw1237@gmail.com>
2023-09-27 16:51:37 +0900

    헤더 푸터 프래그먼트화 다음우편지도 api 부착 timepicker 부착

--- src/main/resources/static/js/member/register.js
+++ src/main/resources/static/js/member/register.js
@@ -139,15 +139,12 @@ function fn_openKakaoMap(){
     new daum.Postcode({
         oncomplete: function(data) {
 
-            var addr = ''; // 주소 변수
-            var extraAddr = ''; // 참고항목 변수
-
-            if (data.userSelectedType === 'R') { // 사용자가 도로명 주소를 선택했을 경우
+            var addr = '';
+            var extraAddr = '';
+            if (data.userSelectedType === 'R') {
                 addr = data.roadAddress;
-            } else { // 사용자가 지번 주소를 선택했을 경우(J)
                 addr = data.jibunAddress;
             }
-
             if(data.userSelectedType === 'R'){
                 if(data.bname !== '' && /[동|로|가]$/g.test(data.bname)){
                     extraAddr += data.bname;
@@ -158,15 +155,12 @@ function fn_openKakaoMap(){
                 if(extraAddr !== ''){
                     extraAddr = ' (' + extraAddr + ')';
                 }
-                document.getElementById("sample6_extraAddress").value = extraAddr;
-
+                document.getElementById("storeAddres").value = extraAddr;
             } else {
-                document.getElementById("sample6_extraAddress").value = '';
+                document.getElementById("storeAddres").value = '';
             }
-
-            document.getElementById('sample6_postcode').value = data.zonecode;
-            document.getElementById("sample6_address").value = addr;
-            document.getElementById("sample6_detailAddress").focus();
+            document.getElementById('storeAddres').value = data.zonecode;
+            document.getElementById("storeAddres").value = addr;
         }
     }).open();
 }
