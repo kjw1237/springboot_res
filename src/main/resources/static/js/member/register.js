1a83f8553f15b330ba70ec725f40d207489db6f3
kjw1237 <kjw1237@gmail.com>
2023-09-27 15:26:02 +0900

    헤더 푸터 프래그먼트화 다음우편지도 api 부착

--- src/main/resources/static/js/member/register.js
+++ src/main/resources/static/js/member/register.js
@@ -134,3 +134,39 @@ birthdayEl.addEventListener('focus', function () {
         }
     }
 })
+
+function fn_openKakaoMap(){
+    new daum.Postcode({
+        oncomplete: function(data) {
+
+            var addr = ''; // 주소 변수
+            var extraAddr = ''; // 참고항목 변수
+
+            if (data.userSelectedType === 'R') { // 사용자가 도로명 주소를 선택했을 경우
+                addr = data.roadAddress;
+            } else { // 사용자가 지번 주소를 선택했을 경우(J)
+                addr = data.jibunAddress;
+            }
+
+            if(data.userSelectedType === 'R'){
+                if(data.bname !== '' && /[동|로|가]$/g.test(data.bname)){
+                    extraAddr += data.bname;
+                }
+                if(data.buildingName !== '' && data.apartment === 'Y'){
+                    extraAddr += (extraAddr !== '' ? ', ' + data.buildingName : data.buildingName);
+                }
+                if(extraAddr !== ''){
+                    extraAddr = ' (' + extraAddr + ')';
+                }
+                document.getElementById("sample6_extraAddress").value = extraAddr;
+
+            } else {
+                document.getElementById("sample6_extraAddress").value = '';
+            }
+
+            document.getElementById('sample6_postcode').value = data.zonecode;
+            document.getElementById("sample6_address").value = addr;
+            document.getElementById("sample6_detailAddress").focus();
+        }
+    }).open();
+}
