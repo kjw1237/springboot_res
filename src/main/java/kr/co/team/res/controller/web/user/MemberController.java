b8a6d5efc47d913adf98b9804b4e39251216e871
kjw1237 <kjw1237@gmail.com>
2023-10-11 17:21:24 +0900

    업데이트 기능구현 및 회원가입 완료페이지 수정 직원관리 db 작성

--- src/main/java/kr/co/team/res/controller/web/user/MemberController.java
+++ src/main/java/kr/co/team/res/controller/web/user/MemberController.java
@@ -43,8 +43,7 @@ public class MemberController extends Base{
         }
     }
 
-    @GetMapping("/api/member/success")
-    public String goSuccess() { return "pages/member/register_success" ;}
+    
 
     @PostMapping("/api/member/verifyduplicateloginid")
     @ResponseBody
