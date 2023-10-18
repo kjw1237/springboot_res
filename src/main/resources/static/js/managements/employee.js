81a4cb849dc377f7050e5ae0cac7aebb99e534a1
kjw1237 <kjw1237@gmail.com>
2023-10-18 18:11:06 +0900

    직원등록 디자인 완료 및 css 수정

--- src/main/resources/static/js/managements/employee.js
+++ src/main/resources/static/js/managements/employee.js
@@ -1,7 +1,8 @@
-function fn_addEmployee(){
-    alert("run");
-}
+$(document).ready(function(){
+    console.log("test");
+});
 
-function fn_addWork(){
-    alert("addWork");
+function fn_onlyNumber(id) {
+    let element = document.getElementById(id);
+    element.value = element.value.replace(/[^0-9]/gi, "");
 }
